708782213580b1642452ad2cc2ee2c0c7c2b303f
Ashish Suryavanshi <ashishsuryavanshi858@gmail.com>
2025-05-25 14:32:50 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,22 @@
 import {products} from "./products.js";
 
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
-},{
+    },{
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
-}];
+    }];
+}
+
+//Whenever we update the cart, we need to save it to localStorage
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
 
 export function addToCart(productId) {
   let matchingItem;
@@ -27,6 +37,8 @@ export function addToCart(productId) {
                 quantity: 1
             });
         }
+
+    saveToStorage();
 }
 
 // Steps:-
@@ -43,4 +55,5 @@ export function removeFromCart(productId) {
     });
 
     cart = newCart;
+    saveToStorage();
 }
