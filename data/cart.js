e72c03ae4e9854a717e46cb88e6c3f84a6a96e80
Ashish Suryavanshi <ashishsuryavanshi858@gmail.com>
2025-05-29 20:23:51 +0530

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -60,3 +60,17 @@ export function removeFromCart(productId) {
     cart = newCart;
     saveToStorage();
 }
+
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+    let matchingItem;
+
+    cart.forEach((CartItem) => {
+        if (productId === CartItem.productId){
+            matchingItem = CartItem
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+    saveToStorage();
+}
